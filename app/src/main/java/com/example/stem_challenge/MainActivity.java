81a0694f686c331116d91e7f87660c2848d6d990
Mirza Mehmedagic <mmehmedagic@cps.edu>
2020-03-03 09:01:18 -0600

    Setup maps API

--- app/src/main/java/com/example/stem_challenge/MainActivity.java
+++ app/src/main/java/com/example/stem_challenge/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.stem_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+
+        LatLng school = new LatLng(41.9454,-87.6907);
+        map.addMarker(new MarkerOptions().position(school).title("Lane Tech"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(school));
+        map.setTrafficEnabled(true);
     }
 }
---
+++ app/src/main/java/com/example/stem_challenge/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.example.stem_challenge;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
