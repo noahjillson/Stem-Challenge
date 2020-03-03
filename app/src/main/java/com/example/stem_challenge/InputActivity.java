be8b1d5a68e3012e36b68b4bc2dee1f6e1160611
Saddat Ahmad <saddatahmad@LT124-26.local>
2020-03-03 09:43:41 -0600

    Added buttons to go to and from home page

--- app/src/main/java/com/example/stem_challenge/InputActivity.java
+++ app/src/main/java/com/example/stem_challenge/InputActivity.java
@@ -1,28 +1,27 @@
 package com.example.stem_challenge;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InputActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.input_button);
+        setContentView(R.layout.input_activity);
+        Button button = findViewById(R.id.home_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, InputActivity.class);
+                Intent i = new Intent(InputActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
-
     }
 
 
